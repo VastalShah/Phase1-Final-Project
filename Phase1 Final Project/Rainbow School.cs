3bcfef2498570aa2db706fc9567bac4f5fecda3a
Vatsal Shah <vashah@teksystems.com>
2021-03-12 10:49:38 +0530

    This class is calling all the methods of Teacher class

---
+++ Phase1 Final Project/Rainbow School.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phase1_Final_Project
+{
+    class Rainbow_School
+    {
+        public static void Do()
+        {
+            Teacher.WriteTeacherData();
+            Teacher.ReadTecherData();
+            Console.WriteLine("Do you want to update teacher data? (yes/no)");
+            string ans = Console.ReadLine();
+            if (ans == "yes")
+            {
+                Console.WriteLine("Enter id of teacher to be updated");
+                int id = Convert.ToInt32(Console.ReadLine());
+                var flag = Teacher.FindAndUpdateTeacher(id);
+                if (flag)
+                {
+                    Teacher.ReadTecherData();
+                }
+                else
+                {
+                    Console.WriteLine("No such teacher record found");
+                }
+            }
+        }
+    }
+}
